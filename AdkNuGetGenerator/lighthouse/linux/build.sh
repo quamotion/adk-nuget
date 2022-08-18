98bdb3a15a396e44986ae66928df82e103624c6c
Frederik Carlier <frederik.carlier@keysight.com>
2022-08-18 15:00:30 +0200

    Remove unused lighthouse project

--- AdkNuGetGenerator/lighthouse/linux/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -shared -o liblighthouse-x86.so -fPIC -ldl lighthouse.c -m32
-gcc -shared -o liblighthouse-x64.so -fPIC -ldl lighthouse.c -m64
-
--- AdkNuGetGenerator/lighthouse/osx/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc -shared -o liblighthouse-x86.dylib -fPIC -ldl lighthouse.c -m32
-gcc -shared -o liblighthouse-x64.dylib -fPIC -ldl lighthouse.c -m64
-
